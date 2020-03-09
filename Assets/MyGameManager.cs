dc069400b45a330bd5d31ae65b6b6042dfee8e52
Dumaru <robinsonmu@unisabana.edu.co>
2020-03-08 20:04:07 -0500

    My Own Scene with Fade In when Loading

---
+++ Assets/MyGameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyGameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image imgFade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Invokes method after 1 second
+        Invoke("Fade", 1);
+    }
+
+    private void Fade()
+    {
+        // In becomes transparent after 2 second no matter the timescale
+        imgFade.CrossFadeAlpha(0, 1, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
