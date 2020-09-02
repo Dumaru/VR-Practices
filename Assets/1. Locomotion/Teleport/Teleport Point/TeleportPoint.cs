32d6bac96c053d228577c70e704542af0dfa6780
Dumaru <robinsonmu@unisabana.edu.co>
2020-09-02 01:20:09 -0500

    Unity 2020 Update with XR Plugin

---
+++ Assets/1. Locomotion/Teleport/Teleport Point/TeleportPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Locomotion
+{
+    /// <summary>
+    /// Calls the teleport manager to teleport the user to this position
+    /// </summary>
+    public class TeleportPoint : MonoBehaviour
+    {
+        public void Teleport()
+        {
+            TeleportManager.Instance.Teleport(transform.position);
+        }
+    }
+
+}
