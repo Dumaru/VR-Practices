2d2032888d9b0f36fdc97139efb89c24929e5d1c
Dumaru <robinsonmu@unisabana.edu.co>
2020-09-13 21:53:20 -0500

    Education Project completed
    There might be some problems with the Timeline packaga, because recent
    some versions are not working, but the latest works fine

---
+++ Assets/4. Educacion Planetas/Scripts/TimelineManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+namespace Education
+{
+
+    public class TimelineManager : MonoBehaviour
+    {
+        [Header("Timeline")]
+        public int actualInteraction = 0;
+        public int[] interactionPoints;
+        public TimelineAsset[] timelineAssets;
+
+        [Header("Complete")]
+        public PlayableDirector playableDirector;
+        public CinemachineDollyCart cart;
+        public CinemachineSmoothPath path;
+
+        private void Start()
+        {
+            Play();
+        }
+
+        public void Play()
+        {
+            StartCoroutine(PlayTimeline());
+        }
+
+        IEnumerator PlayTimeline()
+        {
+            Debug.Log("<b>" + "START" + "</b>");
+            while (actualInteraction < interactionPoints.Length)
+            {
+                if (cart.m_Position > interactionPoints[actualInteraction])
+                {
+                    Debug.Log("Step: " + actualInteraction.ToString());
+                    playableDirector.playableAsset = timelineAssets[actualInteraction];
+                    playableDirector.Play();
+                    actualInteraction++;
+                }
+                yield return null;
+            }
+            Debug.Log("<b>" + "WAITING" + "</b>");
+            while (cart.m_Position < path.PathLength)
+            {
+                yield return null;
+            }
+            Debug.Log("<b>" + "END" + "</b>");
+        }
+
+    }
+}
