3390fefbcfac9ab0805c1a08984f97d92ba7c0e6
Dumaru <robinsonmu@unisabana.edu.co>
2020-09-19 23:30:05 -0500

    Button interaction with reticle pointer and color

---
+++ Assets/5. Videojuego Classroom/Scripts/AnswerButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+    public Color enterColor;
+    public Color exitColor;
+    Image imgButton;
+
+    private void Awake()
+    {
+        imgButton = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerEnter()
+    {
+        Debug.Log("Enter");
+        imgButton.color = enterColor;
+    }
+
+    public void OnPointerExit()
+    {
+        Debug.Log("Exit");
+        imgButton.color = exitColor;
+    }
+}
